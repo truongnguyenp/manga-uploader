679cb20c8c85e690715d00baf249471910ddd8e7
truongnguyenp <truongnguyen310.work@gmail.com>
2023-01-06 01:45:16 +0700

    finish

--- router/index.js
+++ router/index.js
@@ -1,31 +1,39 @@
 const router = require('express').Router();
 const multer = require('multer');
+require('dotenv').config();
+const SharpMulter = require("sharp-multer");
 const path = require('path')
-const host = "http://localhost:3000";
-
+const host = process.env.RENDER_EXTERNAL_HOSTNAME || "http://localhost:3000";
+const LOGO = __dirname + "/images/logo.png";
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+const storage = SharpMulter({
+    destination: (req, file, callback) => callback(null, "./uploads"),
+    imageOptions: {
+        fileFormat: "jpg",
+        quality: 80,
+        resize: { width: 500, height: 500 },
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-
-const upload = multer({ storage: storage }).single("file");
+    watermarkOptions: {
+        input: "./images/logo.png", // watermark image location
+        location: "top-right",
+    },
+});
+const upload = multer({ storage }).array("files", 10);
 router.post('/upload', function (req, res, next) {
     upload(req, res, function (err) {
-        console.log(req.file);
+        const files = req?.files
+        const fileNameLists = files.map((file) => {
+            return `${host}/${file?.filename}`
+        })
+        console.log(JSON.stringify(fileNameLists))
         if (err) {
-            res.json("pls upload jpg file")
+            res.json("err")
         }
         else {
-            res.json(`"${host}/${req.file.filename}"`);
+            res.json(fileNameLists);
         }
     });
 
