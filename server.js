6fa740faf3ad8f182864099e1010d98897033d3f
truongnguyenp <truongnguyen310.work@gmail.com>
2023-01-05 23:36:50 +0700

    v

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const multer = require('multer');
 const fs = require('fs');
 const bodyparser = require(`body-parser`)
@@ -8,7 +9,7 @@ const router = require('./router/index')
 const uploads = path.join(__dirname, 'uploads');
 app.use(bodyparser.json())
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use('/', router)
 app.use(express.static(uploads))
 
